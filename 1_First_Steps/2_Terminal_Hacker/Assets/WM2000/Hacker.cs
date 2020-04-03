5d6639bd6f3a30c413f01d51b5315525e52adb6d
Brandon Hart <Ihartappraisals@gmail.com>
2020-04-02 17:07:34 -0700

    initial commit

---
+++ 1_First_Steps/2_Terminal_Hacker/Assets/WM2000/Hacker.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hacker : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        ShowMainMenu("Brandon");
+    }
+
+    void ShowMainMenu (string name) {
+        Terminal.ClearScreen();
+        Terminal.WriteLine("Hello " + name);
+        Terminal.WriteLine("What would you like to hack into?");
+        Terminal.WriteLine("Press 1 for the local library");
+        Terminal.WriteLine("Press 2 for the local Police Station");
+        Terminal.WriteLine("Enter your selection");  
+    }
+    
+	void OnUserInput(string input) {
+        print(input);
+        }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
